253e529c0d43e5d3202673355069af8dd96ff33c
DarkBlade178 <kacper.piatek2006@gmail.com>
2024-03-13 20:12:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/logowaniesql/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.logowaniesql
+
+import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val root_layout = findViewById<View>(R.id.root_layout)
+        val animDrawable = root_layout.background as AnimationDrawable
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
+    }
+}
